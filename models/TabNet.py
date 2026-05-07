0bc6f8dfd225bf08f21451df4197711dcba7b10c
veronikazaj <102837617+veronikazaj@users.noreply.github.com>
2026-05-07 19:50:02 +0200

    Update and rename TabNet_xai_metriky_valid.py to models/TabNet.py

--- models/TabNet.py
+++ models/TabNet.py
@@ -54,7 +54,7 @@ warnings.filterwarnings("ignore", category=FutureWarning, module="dalex")
 # 0) KONFIG
 # =========================================================
 
-PATH = r"C:\Users\veron\Documents\tuke\Ing.studium\Diplomová práca\data pre programovanie\VEGA_dáta z 13-11-2024-prog\spracovane_Pavol-Almasi-prog\1. vlna imputation.xlsx"
+PATH = "../data.xlsx"
 TARGET = "Závažnosť priebehu ochorenia"
 
 FAST_MODE = True
